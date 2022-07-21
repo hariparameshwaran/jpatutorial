38dcd379a30cc948ae6e04a876b1b342f91781fb
hariparameshwaran <107544423+hariparameshwaran@users.noreply.github.com>
2022-07-21 09:44:49 +0530

    first commit

---
+++ productdata/src/test/java/com/jpatut/springdata/ProductdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpatut.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
